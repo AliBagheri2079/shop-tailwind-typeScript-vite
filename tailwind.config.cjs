2066f9497ed0ea456fc3180e68a9875dafa931fb
Ali Bagheri <alibagheri20ab79@gmail.com>
2022-09-04 22:08:04 +0430

    add header components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 };
